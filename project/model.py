ea2331b0c2ee236387c95f1272c459282b6b3aed
canberkeh <canberkehorozal@gmail.com>
2021-08-22 13:25:24 +0300

    Refactor description model change to destination

--- project/model.py
+++ project/model.py
@@ -1,21 +1,27 @@
-from sqlalchemy.ext.declarative import declarative_base
+'''Database Models'''
 from sqlalchemy import Column, String, Integer, ForeignKey, Text
 from sqlalchemy.orm import relationship
-from project.config.database import Base
+from config.database import Base
 
 
 class Country(Base):
+    '''
+    Country model
+    id : int, pk
+    name : str, not null
+    '''
     __tablename__ = 'country'
 
     id = Column(Integer, primary_key=True)
     country_name = Column(String(80), nullable=False)
-    city_relation = relationship("City", 
-        cascade="all, delete", 
-        passive_deletes=True, 
+    city_relation = relationship("City",
+        cascade="all, delete",
+        passive_deletes=True,
         backref="country"
     )
 
     def serializer(self):
+        '''Returns json result to use in swagger ui'''
         return {
             'id': self.id,
             'name': self.country_name
@@ -23,18 +29,25 @@ class Country(Base):
 
 
 class City(Base):
+    '''
+    City model
+    id : int, pk
+    country_id : int, country relation --> country.id=city.country_id
+    city_name : str, not null
+    '''
     __tablename__ = 'city'
 
     id = Column(Integer, primary_key=True)
     country_id = Column(Integer, ForeignKey('country.id', ondelete="CASCADE"))
     city_name = Column(String(80), nullable=False)
-    description_relation = relationship("Description", 
-        cascade="all, delete", 
-        passive_deletes=True, 
+    destination_relation = relationship("Destination",
+        cascade="all, delete",
+        passive_deletes=True,
         backref="city"
     )
 
     def serializer(self):
+        '''Returns json result to use in swagger ui'''
         return {
             "id": self.id,
             "country_id": self.country_id,
@@ -42,12 +55,15 @@ class City(Base):
         }
 
 
-class Description(Base):
-    __tablename__ = 'description'
+class Destination(Base):
+    '''
+    Destination model
+    id : int, pk
+    city_id : int, relation --> city.id=destination.city_id
+    destination : str, not null
+    '''
+    __tablename__ = 'destination'
 
     id = Column(Integer, primary_key=True)
     city_id = Column(Integer, ForeignKey('city.id', ondelete="CASCADE"))
-    description = Column(Text, nullable=False)
-
-    def __repr__(self):
-        return self.description
+    destination = Column(Text, nullable=False)
