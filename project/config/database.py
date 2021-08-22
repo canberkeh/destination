58f2727dfae607525aa9ab433ec13a2bde058eb4
canberkeh <canberkehorozal@gmail.com>
2021-08-22 17:52:27 +0300

    Refactor database.py

--- project/config/database.py
+++ project/config/database.py
@@ -1,8 +1,10 @@
+'''Database configuration'''
 from sqlalchemy.orm import sessionmaker, scoped_session
 from sqlalchemy import create_engine
 from project.model import Base
 
-engine = create_engine('sqlite:///project/database.db', connect_args={"check_same_thread": False})
+engine = create_engine('sqlite:///project/database.db',
+                       connect_args={"check_same_thread": False})
 
 
 db_session = scoped_session(sessionmaker(autocommit=False,
